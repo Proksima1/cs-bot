4892d2c1c8205dda16fb19ab4f4ec84ee4cb57b5
Konstantin <konstant.2015b@mail.ru>
2022-06-10 17:13:01 +0300

    4

--- app.py
+++ app.py
@@ -3,27 +3,27 @@ from multiprocessing import Process
 from flask import Flask, render_template, request
 from bot import dp, executor, is_pid_alive
 
-server = Flask(__name__)
+app = Flask(__name__)
 
 
-@server.route('/')
+@app.route('/')
 def main():
     print(request.remote_addr)
     return render_template('main.html', ip=request.remote_addr)
 
 
-@server.route('/start_bot', methods=['GET'])
+@app.route('/start_bot', methods=['GET'])
 def start_bot():
-    bot_process = Process(target=before_server_startup)
+    bot_process = Process(target=start_executor)
     bot_process.start()
     pid = bot_process.pid
     print(is_pid_alive(pid))
     return f'{pid}'
 
 
-def before_server_startup():
+def start_executor():
     executor.start_polling(dp, skip_updates=True)
 
 
 if __name__ == '__main__':
-    server.run('0.0.0.0', debug=True)
+    app.run('0.0.0.0', threaded=True, debug=True)
