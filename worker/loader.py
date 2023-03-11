6f1a4e4099642792304f5094a3a318cfb590c80b
Konstantin <konstant.2015b@mail.ru>
2023-03-11 10:47:44 +0300

    Working on worker

--- worker/loader.py
+++ worker/loader.py
@@ -8,6 +8,7 @@ dotenv_path = join(dirname(__file__), 'worker.env')
 load_dotenv(dotenv_path)
 
 LOG_PATH = os.environ.get('LOG_PATH')
+VIP_PATH = os.environ.get('VIP_PATH')
 SERVER = os.environ.get('SERVER')
 USERNAME = os.environ.get('FTP_USERNAME_CONNECT')
 PASSWORD = os.environ.get('FTP_PASSWORD_CONNECT')
--- worker/main.py
+++ worker/main.py
@@ -1,14 +1,16 @@
 import asyncio
+
+from datetime import datetime
 import ftplib
 import re
 from time import sleep
 
-from aioftp import Client
+# from aioftp import Client
 from loader import *
 from logger import MyLogger
 
 
-nowIndex = 200
+nowIndex = 0
 
 def grabFile(session: ftplib.FTP, filename):
     with open(filename, 'wb') as file:
@@ -21,29 +23,48 @@ def placeFile(session: ftplib.FTP, filename):
 
 def doIter():
     # session = ftplib.FTP(SERVER, USERNAME, PASSWORD)
-    # session.cwd(LOG_PATH)
-    # grabFile(session, 'cd_logs.log')
-    getNewData(nowIndex)
+    # download_log(session)
+    # download_vip(session)
+    data = getNewData(nowIndex)
+    data = list(map(lambda x: x[1], data))
+    ids = searchForSteamId(data)
+    print(ids)
 
 
 def getNewData(index):
-    # Open the file and read its contents
     with open('cd_logs.log', 'r', encoding='utf-8', errors='ignore') as f:
         contents = f.read()
-
-    # Use regular expressions to extract the time and Steam ID from each line
     matches = re.findall(r'\[(.*?)\]\s.*?\[(STEAM_[0-1]:[0-1]:\d+)\]', contents)
     if index == -1:
-        time_and_steam_ids = [(match[0], match[1]) for match in matches]
-
-        # Print the list of time and Steam ID pairs
+        time_and_steam_ids = [[match[0], match[1]] for match in matches]
     else:
-        time_and_steam_ids = [(match[0], match[1]) for match in matches[index:]]
-
-        # Print the list of time and Steam ID pairs
-    print(len(time_and_steam_ids))
+        time_and_steam_ids = [[match[0], match[1]] for match in matches[index:]]
+    return time_and_steam_ids
 
 
+def textToDatetime(text) -> datetime:
+    """Converts text to datetime"""
+    return datetime.strptime(text, "%m/%d/%y - %H:%M:%S")
+
+
+def download_log(session):
+    session.cwd(LOG_PATH)
+    grabFile(session, 'cd_logs.log')
+
+
+def download_vip(session):
+    session.cwd('vip/')
+    grabFile(session, 'users.ini')
+
+
+def searchForSteamId(steam_ids):
+    """Gets list of steam ids, searchs in vip file for ids
+       return list of booleans
+    """
+    with open('users.ini', 'r', encoding='utf-8', errors='ignore') as f:
+        file_contents = f.read()
+    o = [(steam_id, re.search(steam_id, file_contents) is not None) for steam_id in steam_ids]
+    return o
 
 
 if __name__ == '__main__':
@@ -51,4 +72,6 @@ if __name__ == '__main__':
     log.start_rotate_logging("WorkerLog", os.path.join('', "worker.log"))
     # while True:
     doIter()
-        # sleep(10)
+    # download_vip()
+    # print(searchForSteamId(['STEAM_0:0:456199604', 'STEAM_0:0:45619960412421']))
+    # sleep(10)
